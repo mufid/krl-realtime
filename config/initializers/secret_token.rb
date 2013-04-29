3d3e36f80a68d6498b44330aa4e090d5f5ab5469
Mufid Afif <mufid@idmail.or.id>
2013-04-29 22:30:11 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KrlRealtime::Application.config.secret_token = '3ec9e8a9dfff679485a475bea3852dbc0d487fd8c9cce94e080fe23f5bd826532189059c1ae77cef7518fa9ce502553509f4385e962f295dc43cc1755f60cec4'
