92d64b6507ccb13bd3b326ba40de5562555cbb57
Mufid Afif <mufid@idmail.or.id>
2013-04-30 07:43:37 +0700

    JSON retrieve OK. Now about to saving to DB

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -7,11 +7,15 @@ class WelcomeController < ApplicationController
     secret_cookie = initial.headers['set-cookie'].split(";").first
     # I dont have any idea with this
     secret_uri = 'http://infoka.krl.co.id/XwNURRRXXFcABg0ZEWNWBQNBVRNTW0JXXgwKCRMBREpBA14FBwFJS0ZQTBNcSwpEWVxBCRt4dyhKVwFXVAcOCAQBCDoxZWJlODFiZA=='
-    @sesuatu = HTTParty.post(secret_uri, :headers => {
+    
+    result = HTTParty.post(secret_uri, :headers => {
       "User-Agent" => "krlrealtimecrawler",
       "Referer" => init_uri,
       "Cookie" => secret_cookie
-    })
+    });
+    
+    @sesuatu = JSON.parse(result) 
+    
     respond_to do |format|
       format.html # index.html.erb
     end
