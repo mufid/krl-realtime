97184cfb9314625d1390a9718d2067b597d1f2e2
Mufid Afif <mufid@idmail.or.id>
2013-05-03 08:29:35 +0700

    Test prepared for stasiun

--- test/unit/stasiun_test.rb
+++ test/unit/stasiun_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
+# No special test for stasiun data
 class StasiunTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should show all stasiun" do
+    gakada = true
+    Stasiun.all.each do |row|
+      Rails::logger.debug row.nama
+      gakada = false
+    end
+    assert !gakada
+  end
+  test "jakarta should exist" do
+    assert !Stasiun.find_by_kode("JAK").nil?
+  end
+  test "bogor should also exist" do
+    assert !Stasiun.find_by_kode("BOO").nil?
+  end
 end
