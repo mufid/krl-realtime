dfb76ea0daad5e968a4116295309a92ee5d56f51
Mufid Afif <mufid@idmail.or.id>
2013-05-04 10:02:50 +0700

    Travis test

--- test/unit/status_berhenti_test.rb
+++ test/unit/status_berhenti_test.rb
@@ -101,7 +101,8 @@ class StatusBerhentiTest < ActiveSupport::TestCase
            cbt_id, :conditions => [
            "no_ka = ?", no_ka ], :order => "waktu DESC").count == 1,
            "Should only show 1 in cilebut after BER add request"
-    assert Status_berhenti.find_by_id(z.id).waktu == waktu,
-           "Should not change the entry"
+    waktu_db = Status_berhenti.find_by_id(z.id).waktu
+    assert waktu_db == waktu,
+           "Should not change the entry. Read from db got #{waktu_db}. Previously got #{waktu}"
   end
 end
