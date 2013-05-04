62742edf8330924bf6a98728c46735eda608c8ff
Mufid Afif <mufid@idmail.or.id>
2013-05-04 09:27:31 +0700

    Passed the test. Now gathering the data

--- app/models/status_berhenti.rb
+++ app/models/status_berhenti.rb
@@ -3,54 +3,39 @@ class Status_berhenti < ActiveRecord::Base
   
   before_save :no_duplicate
   
-  attr_accessible :stasiun_tujuan, :waktu, :no_ka, :id, :ber, :nama_stasiun
-  attr_writer :nama_stasiun
+  attr_accessible :stasiun_tujuan, :waktu, :no_ka, :id
+  attr_accessor :ber, :nama_stasiun
   belongs_to :stasiun
   #has_one :stasiun, :through => :stasiuns_id
   
-  def ber
-    return @ber
-  end
-  
-  def ber= (what)
-    @ber = what
-  end
-  
-  def nama_stasiun
-    return @nama_stasiun
-  end
-  
-  def nama_stasiun= (what)
-    @nama_stasiun = what
-  end
+
   
     
   def no_duplicate
+    # Only run if new_record
     return true unless self.new_record?
-    puts "A new record."
     # Check latest data in that station
     stasiun = Stasiun.find_or_create_by_nama(self.nama_stasiun.capitalize)
     self.stasiun_id = stasiun.id
-    puts "For #{@nama_stasiun}, id is #{stasiun_id}"
     
     # BER phase
-    if @ber
-      terakhir = Status_berhenti.find_by_no_ka(@no_ka, :conditions => 
+    if self.ber
+      terakhir = Status_berhenti.find_by_no_ka(self.no_ka, :conditions => 
       ["waktu > ?", 
-        1.minutes.ago], :order => "waktu DESC")
+        20.minutes.ago], :order => "waktu DESC")
       return false if terakhir.nil?
-      return false if terakhir.stasiun_id == @stasiun_id
+      return false if terakhir.stasiun_id == self.stasiun_id
       return true
     end
 
     # DI phase
     terakhir = Status_berhenti.find_all_by_stasiun_id(stasiun_id, :conditions => 
-    ["waktu > ? AND stasiun_id = ? AND no_ka = ?", 
-      15.minutes.ago, stasiun_id, @no_ka], :order => "waktu DESC", :limit => 2)
+    ["waktu > ? AND no_ka = ?", 
+      15.minutes.ago, self.no_ka], :order => "waktu DESC", :limit => 2)
+        
     return true if terakhir == nil
-    return true if terakhir.count < 2
+    return true if terakhir.length < 2
     Status_berhenti.delete(terakhir.first.id)
-    puts "Something has been discarded"
     true
   end
 end
--- test/unit/status_berhenti_test.rb
+++ test/unit/status_berhenti_test.rb
@@ -2,8 +2,8 @@ require 'test_helper'
 
 # Test for
 class StatusBerhentiTest < ActiveSupport::TestCase
-  # Please consult to fixture
-  test "should show 2 trains stop at bogor" do
+  # Please consult to fixture for this unit test
+  test "should show 2 trains stop at bogor (from fixture)" do
     id_bogor = stasiuns(:bogor).id
     data = Status_berhenti.find_all_by_stasiun_id(id_bogor)
     assert data.count == 2
@@ -16,12 +16,12 @@ class StatusBerhentiTest < ActiveSupport::TestCase
   #     KERETA2 7.49
   # AFTER NEW DATA RETRIEVED
   #   AT BOGOR
-  #     KERETA1 7.45
-  #     KERETA1 7.52
+  #     KERETA1 7.45 <-- The oldest one dibiarin
+  #     KERETA1 7.52 <-- The latest one deleted, replaced with this
   #     KERETA2 7.49  
   test "should show 3 train stop status at bogor" do
     no_ka = status_berhenti(:keretabogor2).no_ka
-    assert true
+
     stasiun_bogor = stasiuns(:bogor).nama
     stasiun_bogor_id = stasiuns(:bogor).id
     stasiun_jakarta_id = stasiuns(:jakarta).id
@@ -41,8 +41,67 @@ class StatusBerhentiTest < ActiveSupport::TestCase
     s.nama_stasiun = stasiun_bogor
     s.stasiun_tujuan = stasiun_jakarta_id
     s.save
-    assert true
-    assert Status_berhenti.where(:stasiun_id => stasiun_bogor_id).count == 3,
-           "Should only show 3 status"
+
+    s = Status_berhenti.new
+    s.no_ka = no_ka
+    s.waktu = 3.seconds.ago
+    s.ber   = false
+    s.nama_stasiun = stasiun_bogor
+    s.stasiun_tujuan = stasiun_jakarta_id
+    s.save
+    
+    c = Status_berhenti.where(:stasiun_id => stasiun_bogor_id, :no_ka => no_ka).count
+    assert c == 2, "Should only show 2 status berhenti instead of #{c}"
+           
+    s = Status_berhenti.new
+    s.no_ka = no_ka
+    s.waktu = 1.seconds.ago
+    s.ber   = false
+    s.nama_stasiun = stasiun_bogor
+    s.stasiun_tujuan = stasiun_jakarta_id
+    s.save
+    
+    assert Status_berhenti.where(:stasiun_id => stasiun_bogor_id, :no_ka => no_ka).count == 2,
+           "Should only show 2 status berhenti, even after next insert"
+
+  end
+  test "should auto update when DI missed" do
+    no_ka = status_berhenti(:keretabogor2).no_ka
+    cbt = stasiuns(:cilebut).nama
+    cbt_id = stasiuns(:cilebut).id
+    bjg = stasiuns(:bojonggede).nama
+    bjg_id = stasiuns(:bojonggede).id
+    jkt_id = stasiuns(:jakarta).id
+    
+    z = Status_berhenti.new
+    z.no_ka = no_ka
+    waktu = 20.seconds.ago
+    z.waktu = 20.seconds.ago
+    z.ber   = true
+    z.nama_stasiun = cbt
+    z.stasiun_tujuan = jkt_id
+    z.save
+    
+    assert !Status_berhenti.find_by_stasiun_id(
+           cbt_id, :conditions => [
+           "no_ka = ?", no_ka ], :order => "waktu DESC").nil?,
+           "Should exist entry in Cilebut"
+
+    s = Status_berhenti.new
+    s.no_ka = no_ka
+    s.waktu = 5.seconds.ago
+    s.ber   = true
+    s.nama_stasiun = cbt
+    s.stasiun_tujuan = jkt_id
+    s.save
+
+    # WARN: Test is not yet contain "not changed", only
+    # test "only show 1 in cilebut"
+    assert Status_berhenti.find_all_by_stasiun_id(
+           cbt_id, :conditions => [
+           "no_ka = ?", no_ka ], :order => "waktu DESC").count == 1,
+           "Should only show 1 in cilebut after BER add request"
+    assert Status_berhenti.find_by_id(z.id).waktu == waktu,
+           "Should not change the entry"
   end
 end
